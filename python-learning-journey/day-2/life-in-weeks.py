3a5f61f0048d10dda5dccf7f0ed9afc0f7177062
Adnan Ali <126818381+adnanali31@users.noreply.github.com>
2023-09-14 13:34:00 +0530

    Create life-in-weeks.py

---
+++ python-learning-journey/day-2/life-in-weeks.py
@@ -0,0 +1,6 @@
+age = int(input("What is your current age? "))
+years_remaining = 90 - age
+days = years_remaining * 365
+weeks = years_remaining * 52
+months = years_remaining * 12
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
